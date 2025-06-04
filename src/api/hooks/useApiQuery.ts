f3136e8ba646d5f5a35bb5a1614559df0ec40df9
akmallmr <akmallmr@gmail.com>
2025-06-05 06:56:46 +0700

    FIX: enhancement types

--- src/api/hooks/useApiQuery.ts
+++ src/api/hooks/useApiQuery.ts
@@ -3,15 +3,15 @@
 import { QueryKey, useQuery, UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
 import api from '../clients';
 
-export function useApiQuery<TData = unknown, TError = Error>(
+export function useApiQuery<TResponse = unknown, TError = Error, TData = TResponse>(
   key: QueryKey,
   url: string,
-  options?: Omit<UseQueryOptions<TData, TError, TData, QueryKey>, 'queryKey' | 'queryFn'>
+  options?: Omit<UseQueryOptions<TResponse, TError, TData, QueryKey>, 'queryKey' | 'queryFn'>
 ): UseQueryResult<TData, TError> {
-  return useQuery<TData, TError, TData, QueryKey>({
+  return useQuery<TResponse, TError, TData, QueryKey>({
     queryKey: key,
     queryFn: async () => {
-      const res = await api.get<TData>(url);
+      const res = await api.get<TResponse>(url);
       return res.data;
     },
     ...options,
